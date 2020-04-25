9a85127b1bcf2562b5dc2bc3cd234bc5493d44c3
sami ben zayed <sam.benzayed@pcc.edu>
2020-04-25 14:04:54 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWord.java
+++ src/edu/pcc/cis/helloworld/HelloWord.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Sami ben Zayed
  * date 04/25/2020
@@ -7,9 +9,10 @@ package edu.pcc.cis.helloworld;
 
 public class HelloWord {
 
-        public static void main(String[] args) {
-            System.out.println("Hello, World!");
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print(new Date());
+    }
     }
 
 
