4b66977cb4b0cd191447fa23e066eab223e76005
sami ben zayed <sam.benzayed@pcc.edu>
2020-04-25 13:56:20 -0700

    added main method that prints heloo world

--- src/edu/pcc/cis/helloworld/HelloWord.java
+++ src/edu/pcc/cis/helloworld/HelloWord.java
@@ -1,4 +1,15 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Sami ben Zayed
+ * date 04/25/2020
+ */
+
 public class HelloWord {
-}
+
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+        }
+    }
+
+
